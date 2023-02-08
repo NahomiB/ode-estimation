16ce61dc091afee59985639a304a9162ca70819c
fvaldes0109 <fvaldes0109@gmail.com>
2023-02-07 20:37:37 -0500

    Generalized model

--- modules/edo_model.py
+++ modules/edo_model.py
@@ -5,54 +5,94 @@ import matplotlib.pyplot as plt
 class EDOModel:
 
 
-    def __init__(self, system, params):
+    def __init__(self, system, restrictions, data):
+
+        self.colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']        
+
         self.system = system
-        self.params = params
+        self.restrictions = restrictions
+        self.D = data
+
+        self.params = self.__obtain_params()
+
+    def __obtain_params(self):
+        
+        mat_size = len(self.restrictions)
+        for eq in self.system:
+            mat_size += len(eq)    
+
+        # Create normal matrix upper part
+        normal_mat = np.zeros((mat_size, mat_size))
+
+        current_row = 0
+        for i in range(len(self.system)): # For each equation...
+            for j in range(len(self.system[i])): # ...and for each parameter...
+                for k in range(j, len(self.system[i])): # ...and for each parameter again...
+
+                    normal_mat[current_row + j][current_row + k] = self.__scalar_product(self.system[i][j], self.system[i][k], self.D)
+                    normal_mat[current_row + k][current_row + j] = normal_mat[current_row + j][current_row + k]
+            current_row += len(self.system[i])
+        
+        # Embed restrictions into normal matrix
+        row = mat_size - len(self.restrictions)
+        for r in self.restrictions:
+            normal_mat[row][r[0]] = 1
+            normal_mat[row][r[1]] = -1
+            normal_mat[r[0]][row] = 1
+            normal_mat[r[1]][row] = -1
+            row += 1
+
+        # Create right side vector for the linear equations system
+        b = np.zeros(6)
+
+        cont = 0
+        for eq in range(len(self.system)):
+            for param in range(len(self.system[eq])):
+                b[cont] += self.__scalar_product_deriv(eq + 1, self.system[eq][param], self.D)
+                cont += 1
+
+        # Solve the system
+        x = np.linalg.solve(normal_mat, b)
+        return x
 
 
-    def get_model(self, y, t, a, b):
+    def get_model(self, y, t):
 
-        S = y[0]
-        I = y[1]
-        R = y[2]
-        dSdt = -a * S * I
-        dIdt = a * S * I - b * I
-        dRdt = b * I
-        return [dSdt, dIdt, dRdt]
+        result = []
+        cont = 0
+        for equation in self.system:
+            right_member = 0
+            for f in equation:
+                right_member += self.params[cont] * f(t, *y)
+                cont += 1
+            result.append(right_member)
+
+        return result
 
 
     def graph(self):
-        # Define the parameters
-        a = 0.3
-        b = 0.2
-        S0 = 0.99
-        I0 = 0.01
-        R0 = 0.0
 
-        # Define the time span
-        t = np.linspace(0, 160, 160)
-
+        t = np.linspace(0, len(self.D), len(self.D))
+        
         # Solve the ODEs
         from scipy.integrate import odeint
-        y0 = [S0, I0, R0]
-        ret = odeint(self.get_model, y0, t, args=(a, b))
-        test = odeint(self.get_model, y0, t, args=())
-        S, I, R = ret.T
-
-        # for i in range(0, 160):
-        #     print(f'{i}: {S[i]}, {I[i]}, {R[i]}')
+        y0 = self.D[0][1:]
+        ret = odeint(self.get_model, y0, t)
+        Y = ret.T
 
         # Plot the data on three separate curves for S(t), I(t) and R(t)
         fig = plt.figure(facecolor='w')
         ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-        ax.plot(t, S, 'b', alpha=0.5, lw=2, label='Susceptible')
-        ax.plot(t, I, 'r', alpha=0.5, lw=2, label='Infected')
-        ax.plot(t, R, 'g', alpha=0.5, lw=2, label='Recovered')
+        for i in range(len(Y)):
+            ax.plot(t, Y[i], self.colors[i % len(self.colors)], alpha=0.5, lw=2, label='y(t)')
 
+        # Get a subset of the data to plot
+        data_subset = self.D[::10]
 
+        # Plot the data as points
+        for i in range(len(Y)):
+            ax.plot(data_subset[:, 0], data_subset[:, i + 1], self.colors[i % len(self.colors)] + 'o')
 
-        ax.set_xlabel('Time /days')
-        ax.set_ylabel('Number (1000s)')
         ax.set_ylim(0,1.2)
         ax.yaxis.set_tick_params(length=0)
         ax.xaxis.set_tick_params(length=0)
@@ -62,3 +102,21 @@ class EDOModel:
         for spine in ('top', 'right', 'bottom', 'left'):
             ax.spines[spine].set_visible(False)
         plt.show()
+
+
+    def __scalar_product(self, f, g, D):
+
+        sum = 0
+        for i in range(len(D)):
+            sum += f(D[i][0], D[i][1], D[i][2], D[i][3]) * g(D[i][0], D[i][1], D[i][2], D[i][3])
+        return sum
+
+
+    def __scalar_product_deriv(self, col, f, D):
+
+        sum = 0
+        for i in range(len(D) - 1):
+            m = (D[i + 1][col] - D[i][col]) / (D[i + 1][0] - D[i][0])
+            sum += f(D[i][0], D[i][1], D[i][2], D[i][3]) * m
+        return sum
+
--- tester.py
+++ tester.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from modules.edo_model import EDOModel
+
+
 def scalar_product(f, g, D):
 
     sum = 0
@@ -30,38 +33,8 @@ system = [[f_11],
           [f_21, f_22],
           [f_31]]
 
-normal_mat = np.zeros((6, 6))
+restrictions = [[0, 1], [2, 3]]
 
-current_row = 0
-for i in range(3): # For each equation...
-    for j in range(len(system[i])): # ...and for each parameter...
-        for k in range(j, len(system[i])): # ...and for each parameter again...
+sir = EDOModel(system, restrictions, D)
+sir.graph()
 
-            normal_mat[current_row + j][current_row + k] = scalar_product(system[i][j], system[i][k], D)
-            normal_mat[current_row + k][current_row + j] = normal_mat[current_row + j][current_row + k]
-    current_row += len(system[i])
-
-normal_mat[4][0] = 1
-normal_mat[4][1] = -1
-normal_mat[0][4] = 1
-normal_mat[1][4] = -1
-
-normal_mat[5][2] = 1
-normal_mat[5][3] = -1
-normal_mat[2][5] = 1
-normal_mat[3][5] = -1
-
-print(normal_mat)
-
-# Vector of size 6 full of 0
-v = np.zeros(6)
-
-cont = 0
-for eq in range(len(system)):
-    for param in range(len(system[eq])):
-        v[cont] += scalar_product_deriv(eq + 1, system[eq][param], D)
-        cont += 1
-
-# Solve the system
-x = np.linalg.solve(normal_mat, v)
-print(x)
