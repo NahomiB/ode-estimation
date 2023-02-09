9bc0cd74b7ea0df07f156192f1eed0ab9ee9e5d9
fvaldes0109 <fvaldes0109@gmail.com>
2023-02-08 20:37:44 -0500

    Remove unnecesary methods

--- estimate.py
+++ estimate.py
@@ -3,24 +3,6 @@ import matplotlib.pyplot as plt
 
 from modules.edo_model import EDOModel
 
-
-def scalar_product(f, g, D):
-
-    sum = 0
-    for i in range(len(D)):
-        sum += f(D[i][0], D[i][1], D[i][2], D[i][3]) * g(D[i][0], D[i][1], D[i][2], D[i][3])
-    return sum
-
-
-def scalar_product_deriv(col, f, D):
-
-    sum = 0
-    for i in range(len(D) - 1):
-        m = (D[i + 1][col] - D[i][col]) / (D[i + 1][0] - D[i][0])
-        sum += f(D[i][0], D[i][1], D[i][2], D[i][3]) * m
-    return sum
-
-
 # Load points data in /data/sir_model.csv
 D = np.loadtxt('data/sir_model.csv', delimiter=',', skiprows=1)
 
